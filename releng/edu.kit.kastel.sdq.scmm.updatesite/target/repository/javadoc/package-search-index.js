bd7068f018933af1ac7a11fefa896c908a74eb77
Sandro <layornos@gmail.com>
2020-12-22 15:22:33 +0100

    rm target folders

--- releng/edu.kit.kastel.sdq.scmm.updatesite/target/repository/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"simulatorComponentMetamodel"},{"l":"simulatorComponentMetamodel.impl"},{"l":"simulatorComponentMetamodel.util"}]
